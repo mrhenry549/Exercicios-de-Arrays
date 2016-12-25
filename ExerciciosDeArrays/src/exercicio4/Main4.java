9f9ffe03211a9f38a78523e867821754d6a74036
Henrique <Henrique@Henrique-PC>
2016-12-25 18:22:12 +0000

    Exercicio 4

---
+++ ExerciciosDeArrays/src/exercicio4/Main4.java
@@ -0,0 +1,25 @@
+package exercicio4;
+
+import java.util.ArrayList;
+
+public class Main4 {
+    
+    ArrayList<Integer> arrayNumeros;
+    int soma = 0;
+    
+    public Main4(){
+        
+        arrayNumeros = new ArrayList<Integer>();
+        
+        arrayNumeros.add(3);
+        arrayNumeros.add(5);
+        arrayNumeros.add(6);
+        arrayNumeros.add(1);
+        
+        for (int i=0; i<arrayNumeros.size(); i++){
+            soma = soma + arrayNumeros.get(i);
+        }
+        
+        System.out.println("TOTAL: "+soma);
+    }
+}
---
+++ ExerciciosDeArrays/src/exerciciosdearrays/ExerciciosDeArrays.java
@@ -0,0 +1,11 @@
+package exerciciosdearrays;
+
+import exercicio4.Main4;
+
+public class ExerciciosDeArrays {
+
+    public static void main(String[] args) {
+        Main4 m4 = new Main4();
+    }
+    
+}
