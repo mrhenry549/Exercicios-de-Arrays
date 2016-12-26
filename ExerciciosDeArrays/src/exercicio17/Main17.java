7afb37c5f3ad75d544e96303b70eb2b5de8c29e8
Henrique <Henrique@Henrique-PC>
2016-12-26 13:27:42 +0000

    Updates ao Exercicio 17

--- ExerciciosDeArrays/src/exercicio17/Main17.java
+++ ExerciciosDeArrays/src/exercicio17/Main17.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 public class Main17 {
     
-    int numero, media = 0, soma = 0;
-    Scanner s;
+    double numero, media = 0, soma = 0;
+    Scanner s = new Scanner(System.in);
     
     public Main17(){
-        ArrayList<Integer> arrayNumeros = new ArrayList<>();
+        ArrayList<Double> arrayNumeros = new ArrayList<>();
         
         System.out.println("Introduza 2 números inteiros à escolha");
         
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 2; i++){
             numero = s.nextInt();
             arrayNumeros.add(numero);
             soma = soma + arrayNumeros.get(i);
